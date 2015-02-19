a0401d3e7b687d765e452b9c21d72d6d608f7725
Matt Faulkner <mnfaulk@gmail.com>
2015-02-18 17:00:53 -0800

    Removes scala client, and switches to openjdk-7-jre-headless

--- run-go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p $GOPATH/src/github.com/stealthly
-cd $GOPATH/src/github.com/stealthly
-go get github.com/stealthly/go-kafka
-cd $GOPATH/src/github.com/stealthly/go-kafka
-go run scala_go_kafka.go $1 $2
